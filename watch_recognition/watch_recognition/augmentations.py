add watch rotation model, fix bugs with metrics, automatically rotate cropped clocks

watch_recognition/watch_recognition/augmentations.py
@@ -14,18 +14,18 @@ DEFAULT_TRANSFORMS = A.Compose(
         A.RandomSizedCrop(min_max_height=(200, 224), height=224, width=224, p=0.5),
         A.ShiftScaleRotate(
             p=0.8,
-            # rotate_limit=180, # TODO change to max 45 angle
+            rotate_limit=90,
         ),
         A.OneOf(
             [
                 A.HueSaturationValue(p=0.7),
                 A.RGBShift(p=0.7),
                 A.ChannelShuffle(p=0.7),
+                A.RandomBrightnessContrast(p=0.7),
             ],
             p=1,
         ),
-        A.MotionBlur(blur_limit=20),
-        A.RandomBrightnessContrast(),
+        A.MotionBlur(),
     ],
     # format="xyas" is required while using tf.Data pipielines, otherwise
     # tf cannot validate the output shapes # TODO check if this is correct
@@ -196,7 +196,7 @@ def encode_keypoints_to_mask(
 def add_sample_weights(image, label):
     # The weights for each class, with the constraint that:
     #     sum(class_weights) == 1.0
-    class_weights = tf.constant([1, 1, 1, 5e-3])
+    class_weights = tf.constant([1, 1, 1, 1e-3])
     class_weights = class_weights / tf.reduce_sum(class_weights)
 
     # Create an image of `sample_weights` by using the label at each pixel as an

watch_recognition/watch_recognition/models.py
@@ -73,7 +73,7 @@ def extract_points_from_map(
     predicted_map,
     detection_threshold=0.5,
     text_threshold=0.5,
-    size_threshold=1,
+    size_threshold=2,
 ) -> List[Point]:
     """
     Inspired by keras-ocr segmentation to bboxes code
@@ -101,8 +101,8 @@ def extract_points_from_map(
         if size < size_threshold:
             continue
 
-        mean_score = np.max(predicted_map[labels == component_id])
-        if mean_score < detection_threshold:
+        score = np.max(predicted_map[labels == component_id])
+        if score < detection_threshold:
             continue
 
         segmap = np.where(
@@ -110,15 +110,22 @@ def extract_points_from_map(
         )
 
         box_center = np.array(decode_single_point(segmap))
-        points.append(Point(*box_center, score=float(mean_score)))
+        points.append(Point(*box_center, score=float(score)))
     return points
 
 
-def convert_outputs_to_keypoints(
-    predicted,
-) -> Tuple[Point, Point, Point, Point]:
-
-    masks = predicted.transpose((2, 0, 1))
+def convert_outputs_to_keypoints(predicted) -> Tuple[Point, Point, Point, Point]:
+    argmax_masks = predicted.argmax(axis=-1)
+    new_masks = []
+    for label in range(4):
+        new_mask = np.where(
+            argmax_masks == label,
+            predicted[:, :, label],
+            np.zeros_like(predicted[:, :, label]),
+        )
+        new_masks.append(new_mask)
+    masks = np.stack(new_masks, axis=-1)
+    masks = masks.transpose((2, 0, 1))
     center_points = extract_points_from_map(masks[0])
     if not center_points:
         center_points = [Point.none()]
@@ -132,11 +139,12 @@ def convert_outputs_to_keypoints(
     top = sorted(top_points, key=lambda x: x.score)[-1]
     top = dataclasses.replace(top, name="Top")
     # Hands
+    hands_points = extract_points_from_map(masks[2], size_threshold=3)
 
-    hands_points = extract_points_from_map(masks[2])
     if not hands_points:
         hands_points = [Point.none(), Point.none()]
-    # TODO sort by distance to center instead of score?
+    if len(hands_points) == 1:
+        hands_points = (hands_points[0], hands_points[0])
     hands_points = sorted(hands_points, key=lambda x: x.score)[-2:]
 
     hour, minute = get_minute_and_hour_points(center, tuple(hands_points))

watch_recognition/watch_recognition/reports.py
@@ -5,24 +5,8 @@ from typing import Tuple
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-from IPython.core.display import display
 
 from watch_recognition.models import convert_outputs_to_keypoints, points_to_time
-
-
-def predict_on_image(path, model, image_size=(100, 100)):
-    test_image = tf.keras.preprocessing.image.load_img(
-        path, "rgb", target_size=image_size, interpolation="bicubic"
-    )
-    test_image = tf.keras.preprocessing.image.img_to_array(test_image)
-    test_image = np.array([test_image])
-    pred = model.predict(test_image)
-    pred["minute"] = np.zeros_like(pred["hour"])
-    for i, (h_pred, m_pred) in enumerate(zip(pred["hour"], pred["minute"])):
-        pred_hour = np.round((h_pred * 20) + 6)[0]
-        pred_minutes = 0  # np.round((m_pred + 0.5) * 60)[0]
-        print(pred_hour, ":", pred_minutes)
-    display(tf.keras.preprocessing.image.array_to_img(test_image[0]))
 
 
 def plot_to_image(figure):
@@ -134,7 +118,18 @@ def run_on_image_debug(model, image):
         for p in (center, top, hour, minute)
     ]
     downsample_factor = image.shape[1] / predicted.shape[1]
-    masks = predicted.transpose((2, 1, 0))
+    argmax_masks = predicted.argmax(axis=-1)
+    new_masks = []
+    for label in np.unique(argmax_masks):
+        new_mask = np.where(
+            argmax_masks == label,
+            predicted[:, :, label],
+            np.zeros_like(predicted[:, :, label]),
+        )
+        new_masks.append(new_mask)
+    masks = np.stack(new_masks, axis=-1)
+
+    masks = masks.transpose((2, 1, 0))
     extent = [0, predicted.shape[1], predicted.shape[1], 0]
     fig, ax = plt.subplots(3, 2, figsize=(5, 10))
     # Center

watch_recognition/watch_recognition/utilities.py
@@ -2,7 +2,6 @@ import dataclasses
 from typing import Optional, Tuple
 
 import numpy as np
-from matplotlib import pyplot as plt
 
 
 @dataclasses.dataclass(frozen=True)
@@ -21,6 +20,12 @@ class Point:
 
     def translate(self, x: float, y: float) -> "Point":
         return Point(self.x + x, self.y + y, self.name, self.score)
+
+    def distance(self, other: "Point") -> float:
+        diff = np.array(self.as_coordinates_tuple) - np.array(
+            other.as_coordinates_tuple
+        )
+        return float(np.sqrt((diff ** 2).sum()))
 
     def rotate_around_origin_point(self, origin: "Point", angle: float) -> "Point":
         theta = np.radians(angle)
@@ -67,6 +72,24 @@ class BBox:
     def scale(self, x: float, y: float) -> "BBox":
         return BBox(
             self.x_min * x, self.y_min * y, self.x_max * x, self.y_max * y, self.name
+        )
+
+    @property
+    def center(self) -> "Point":
+        return Point((self.x_max + self.x_min) / 2, (self.y_max + self.y_min) / 2)
+
+    def center_scale(self, x: float, y: float) -> "BBox":
+        w, h = self.width * x, self.height * y
+        cx, cy = self.center.x, self.center.y
+
+        x_min = cx - w / 2
+        x_max = cx + w / 2
+
+        y_min = cy - h / 2
+        y_max = cy + h / 2
+
+        return dataclasses.replace(
+            self, x_min=x_min, x_max=x_max, y_min=y_min, y_max=y_max
         )
 
     @property