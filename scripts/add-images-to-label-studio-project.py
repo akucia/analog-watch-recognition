fc11a824444b0251a2f5917545138caedec807aa
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-27 07:20:04 +0000

    Adding tarfile member sanitization to extractall()

--- scripts/add-images-to-label-studio-project.py
+++ scripts/add-images-to-label-studio-project.py
@@ -61,7 +61,29 @@ def download_and_uzip_model(url: str, save_dir: str = "/tmp/") -> Path:
     extract_dir = save_dir / save_file.stem
     with tarfile.open(save_file) as tar:
         logging.debug(f"extracting {name}")
-        tar.extractall(extract_dir)
+        
+        import os
+        
+        def is_within_directory(directory, target):
+        	
+        	abs_directory = os.path.abspath(directory)
+        	abs_target = os.path.abspath(target)
+        
+        	prefix = os.path.commonprefix([abs_directory, abs_target])
+        	
+        	return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+        	for member in tar.getmembers():
+        		member_path = os.path.join(path, member.name)
+        		if not is_within_directory(path, member_path):
+        			raise Exception("Attempted Path Traversal in Tar File")
+        
+        	tar.extractall(path, members, numeric_owner=numeric_owner) 
+        	
+        
+        safe_extract(tar, extract_dir)
 
     return extract_dir
 
